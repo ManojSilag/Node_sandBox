42b1912cac9f6d6d2eb6761f45269553f1913ad0
ManojSilag <manojsilag@gmail.com>
2020-04-24 13:35:14 +0530

    CRUD mongodb

--- section5/mongodb.js
+++ section5/mongodb.js
@@ -1,5 +1,12 @@
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
+// const ObjectId = mongodb.ObjectID;
+
+const { MongoClient, ObjectID } = require("mongodb");
+
+// const id = new ObjectID();
+// console.log('dev: id', id.id.length)
+// console.log('dev: id', id.toHexString().length);
 
 const connectURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
@@ -12,57 +19,64 @@ MongoClient.connect(
       console.log(error);
       return console.log("Unable to connect to database");
     }
-
     const db = client.db(databaseName);
 
-    // db.collection("users").insertOne(
-    //   {
-    //     name: "Manoj",
-    //     age: 23,
-    //   },
-    //   (error, result) => {
-    //       if(error)
-    //       return console.log(error, "Unable to insert");
-    //   console.log(result.ops);
-    //   }
-    // );
+    // Update---------------------------------------------------------------
 
-    // db.collection("users").insertMany(
-    //   [
+    // db.collection("users")
+    //   .updateOne(
     //     {
-    //       name: "Rohit",
-    //       age: 24,
+    //       _id: new ObjectID("5ea14464058daf2045faf9f9"),
     //     },
     //     {
-    //       name: "pranav",
-    //       age: 26,
-    //       fullname: "Pranav gim",
-    //     },
-    //   ],
-    //   (error, result) => {
-    //       if (error) return console.log(error, "Unable to insert");
-    //       console.log(result.ops);
-    //   }
-    // );
+    //       $inc: {
+    //         age: 34,
+    //       },
+    //     }
+    //   )
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
 
-    db.collection("tasks").insertMany([
-      {
-        description: "Eat Breakfast",
-        completed: true,
-      },
-      {
-        description: "Exercise",
-        completed: false,
-      },
-      {
-        description: "Learn nodejs",
-        completed: true,
-      }
-    ],(error, result) => {
-        if(error)
-        return console.log(error, "Unable to insert");
-        
-        console.log(result.ops);
-    });
+    // db.collection("tasks")
+    //   .updateMany(
+    //     {
+    //       completed: false,
+    //     },
+    //     {
+    //       $set: {
+    //         completed: true,
+    //       },
+    //     }
+    //   )
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+
+    // delete---------------------------------------
+
+    // db.collection('users').deleteMany({
+    //   age: 26
+    // }).then((result)=>{
+    //   console.log(result);
+    // }).catch((error)=>{
+    //   console.log(error);
+    // })
+
+    db.collection('tasks').deleteOne({
+      description : "Learn nodejs"
+    }).then((result)=>{
+      console.log(result);
+    }).catch((error)=>{
+      console.log(error);
+    })
+
   }
 );
