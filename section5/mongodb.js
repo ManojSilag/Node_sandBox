6e53530017fabb93bf601d9f612917602eb397cf
ManojSilag <manojsilag@gmail.com>
2020-04-26 17:03:44 +0530

    update

--- section5/mongodb.js
+++ section5/mongodb.js
@@ -1,81 +1,81 @@
-// const mongodb = require("mongodb");
-// const MongoClient = mongodb.MongoClient;
-// const ObjectId = mongodb.ObjectID;
+// // const mongodb = require("mongodb");
+// // const MongoClient = mongodb.MongoClient;
+// // const ObjectId = mongodb.ObjectID;
 
-const { MongoClient, ObjectID } = require("mongodb");
+// const { MongoClient, ObjectID } = require("mongodb");
 
-// const id = new ObjectID();
-// console.log('dev: id', id.id.length)
-// console.log('dev: id', id.toHexString().length);
+// // const id = new ObjectID();
+// // console.log('dev: id', id.id.length)
+// // console.log('dev: id', id.toHexString().length);
 
-const connectURL = "mongodb://127.0.0.1:27017";
-const databaseName = "task-manager";
+// const connectURL = "mongodb://127.0.0.1:27017";
+// const databaseName = "task-manager";
 
-MongoClient.connect(
-  connectURL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (error, client) => {
-    if (error) {
-      console.log(error);
-      return console.log("Unable to connect to database");
-    }
-    const db = client.db(databaseName);
+// MongoClient.connect(
+//   connectURL,
+//   { useNewUrlParser: true, useUnifiedTopology: true },
+//   (error, client) => {
+//     if (error) {
+//       console.log(error);
+//       return console.log("Unable to connect to database");
+//     }
+//     const db = client.db(databaseName);
 
-    // Update---------------------------------------------------------------
+//     // Update---------------------------------------------------------------
 
-    // db.collection("users")
-    //   .updateOne(
-    //     {
-    //       _id: new ObjectID("5ea14464058daf2045faf9f9"),
-    //     },
-    //     {
-    //       $inc: {
-    //         age: 34,
-    //       },
-    //     }
-    //   )
-    //   .then((result) => {
-    //     console.log(result);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
+//     // db.collection("users")
+//     //   .updateOne(
+//     //     {
+//     //       _id: new ObjectID("5ea14464058daf2045faf9f9"),
+//     //     },
+//     //     {
+//     //       $inc: {
+//     //         age: 34,
+//     //       },
+//     //     }
+//     //   )
+//     //   .then((result) => {
+//     //     console.log(result);
+//     //   })
+//     //   .catch((error) => {
+//     //     console.log(error);
+//     //   });
 
-    // db.collection("tasks")
-    //   .updateMany(
-    //     {
-    //       completed: false,
-    //     },
-    //     {
-    //       $set: {
-    //         completed: true,
-    //       },
-    //     }
-    //   )
-    //   .then((result) => {
-    //     console.log(result);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
+//     // db.collection("tasks")
+//     //   .updateMany(
+//     //     {
+//     //       completed: false,
+//     //     },
+//     //     {
+//     //       $set: {
+//     //         completed: true,
+//     //       },
+//     //     }
+//     //   )
+//     //   .then((result) => {
+//     //     console.log(result);
+//     //   })
+//     //   .catch((error) => {
+//     //     console.log(error);
+//     //   });
 
 
-    // delete---------------------------------------
+//     // delete---------------------------------------
 
-    // db.collection('users').deleteMany({
-    //   age: 26
-    // }).then((result)=>{
-    //   console.log(result);
-    // }).catch((error)=>{
-    //   console.log(error);
-    // })
+//     // db.collection('users').deleteMany({
+//     //   age: 26
+//     // }).then((result)=>{
+//     //   console.log(result);
+//     // }).catch((error)=>{
+//     //   console.log(error);
+//     // })
 
-    db.collection('tasks').deleteOne({
-      description : "Learn nodejs"
-    }).then((result)=>{
-      console.log(result);
-    }).catch((error)=>{
-      console.log(error);
-    })
-  }
-);
+//     db.collection('tasks').deleteOne({
+//       description : "Learn nodejs"
+//     }).then((result)=>{
+//       console.log(result);
+//     }).catch((error)=>{
+//       console.log(error);
+//     })
+//   }
+// );
