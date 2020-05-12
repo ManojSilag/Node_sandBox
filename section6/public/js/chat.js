d68d7456f4f05470589e53a28776c359f5a1d1f0
ManojSilag <manojsilag@gmail.com>
2020-05-12 13:46:01 +0530

    socket events

--- section6/public/js/chat.js
+++ section6/public/js/chat.js
@@ -1,1 +1,26 @@
-io()
+const socket = io();
+
+socket.on("message", (data) => {
+  console.log(data);
+});
+
+const input = document.querySelector("#input");
+const button = document.querySelector("#send");
+const send_location = document.querySelector("#send-location");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  const value = input.value;
+  socket.emit("sendMessage", value);
+});
+
+send_location.addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolaction is not supported by your browser");
+  }
+  navigator.geolocation.getCurrentPosition((postion) => {
+    const { latitude, longitude } = postion.coords;
+    const location = `Location: lat: ${latitude}, long: ${longitude}`;
+    socket.emit("sendLocation", location);
+  });
+});
--- section6/src/index.js
+++ section6/src/index.js
@@ -7,14 +7,35 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const publicDirectoryPath = path.join(__dirname, "../public/");
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
-   console.log('new io connection');   
-})
+io.on("connection", (socket) => {
+  console.log("new io connection");
+
+  socket.emit("message", "Welcome!!");
+  socket.broadcast.emit("message", "A new user has joined");
+
+  socket.on("sendMessage", (message) => {
+    console.log("server: message", message);
+    io.emit("message", message);
+  });
+
+  socket.on("sendLocation", (message) => {
+    console.log("dev: message", message);
+    const { latitude, longitude } = message;
+    socket.broadcast.emit(
+      "message",
+      `https://google.com/maps?q=${latitude},${longitude}`
+    );
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left");
+  });
+});
 
 server.listen(port, () => {
   console.log("app listing on ", port);
